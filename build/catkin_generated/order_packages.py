93f8ca407fc943ed820a8ddc27f2c924627a6b88
Zvezdana Arsovska <arsovska93@gmail.com>
2017-06-05 00:26:39 +0300

    3

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/arsovska/RobTech/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/arsovska/RobTech/devel;/opt/ros/indigo".split(';') if "/home/arsovska/RobTech/devel;/opt/ros/indigo" != "" else []
---
+++ build/my_r2d2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/arsovska/RobTech/src/my_r2d2
+# Build directory: /home/arsovska/RobTech/build/my_r2d2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
